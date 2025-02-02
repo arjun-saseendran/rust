091e3793addb79a3785894be6916640e28c5836a
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-02 21:57:02 +0530

    variabe change to mutable

--- day01/variables/src/main.rs
+++ day01/variables/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     let number = 10;
     println!("The number is: {}" ,number);
+    let mut name = "Arjun Saseendran";
+    println!("My name is: {} ", name);
+    name = "Rust";
+    println!("My name is: {} ",name);
 }
