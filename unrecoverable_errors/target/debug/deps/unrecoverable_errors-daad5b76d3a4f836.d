3d933860a7fd5939ae7b33f2c14fa4256c62075f
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-09 16:36:18 +0530

    add unrecoverable errors

---
+++ unrecoverable_errors/target/debug/deps/unrecoverable_errors-daad5b76d3a4f836.d
@@ -0,0 +1,5 @@
+/Users/arjunsaseendran/Developer/rust/unrecoverable_errors/target/debug/deps/unrecoverable_errors-daad5b76d3a4f836: src/main.rs
+
+/Users/arjunsaseendran/Developer/rust/unrecoverable_errors/target/debug/deps/unrecoverable_errors-daad5b76d3a4f836.d: src/main.rs
+
+src/main.rs:
---
+++ unrecoverable_errors/target/debug/unrecoverable_errors.d
@@ -0,0 +1,1 @@
+/Users/arjunsaseendran/Developer/rust/unrecoverable_errors/target/debug/unrecoverable_errors: /Users/arjunsaseendran/Developer/rust/unrecoverable_errors/src/main.rs
