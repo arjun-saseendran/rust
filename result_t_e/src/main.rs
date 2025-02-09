d70efab46e0677a8f030878c2b0c74dc647b23c4
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-09 17:22:37 +0530

    add update

--- result_t_e/src/main.rs
+++ result_t_e/src/main.rs
@@ -2,6 +2,6 @@ use std::fs;
 
 
 fn main() {
-let content = fs::read_to_string("hello.txt");
+let content = fs::read_to_string("hell.txt").expect("Item not found!");
     println!("The content is: {:?}",content);
 }
