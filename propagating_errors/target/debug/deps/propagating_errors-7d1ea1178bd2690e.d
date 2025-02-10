6738ea0fcbf39d2bcddc4c8336004026aed5deb2
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-10 14:32:35 +0530

    add propagating_errors

---
+++ propagating_errors/target/debug/deps/propagating_errors-7d1ea1178bd2690e.d
@@ -0,0 +1,5 @@
+/Users/arjunsaseendran/Developer/rust/propagating_errors/target/debug/deps/propagating_errors-7d1ea1178bd2690e: src/main.rs
+
+/Users/arjunsaseendran/Developer/rust/propagating_errors/target/debug/deps/propagating_errors-7d1ea1178bd2690e.d: src/main.rs
+
+src/main.rs:
